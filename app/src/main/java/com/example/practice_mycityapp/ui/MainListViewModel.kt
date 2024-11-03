f912bf132659cb4a498908cfec7419dc033c5865
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-03 17:51:10 +0000

    Implements View Model main functionalities

--- app/src/main/java/com/example/practice_mycityapp/ui/MainListViewModel.kt
+++ app/src/main/java/com/example/practice_mycityapp/ui/MainListViewModel.kt
@@ -2,19 +2,73 @@ package com.example.practice_mycityapp.ui
 
 import androidx.lifecycle.ViewModel
 import com.example.practice_mycityapp.data.DataProvider
+import com.example.practice_mycityapp.model.Building
+import com.example.practice_mycityapp.model.Resource
 import com.example.practice_mycityapp.utils.ContentType
+import com.example.practice_mycityapp.utils.View
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
 class MainListViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(
         MainListUIState(
             selectedList = ContentType.BUILDINGS,
             selectedBuilding = DataProvider.buildings[0],
-            selectedResource = DataProvider.resources[0]
+            selectedResource = DataProvider.resources[0],
+            view = View.LIST_VIEW
         )
     )
     val uiState: StateFlow<MainListUIState> = _uiState
 
+    fun resetUIState(){
+        _uiState.value = MainListUIState(
+            selectedList = ContentType.BUILDINGS,
+            selectedBuilding = DataProvider.buildings[0],
+            selectedResource = DataProvider.resources[0],
+            view = View.LIST_VIEW
+        )
+    }
+
+    fun selectResource(id: Int){
+        val resource = findResource(id) ?: DataProvider.resources[0]
+
+        _uiState.update { currentState ->
+            currentState.copy(
+                selectedResource = resource
+            )
+        }
+    }
+
+    fun selectBuilding(id: Int){
+        val building = findBuilding(id) ?: DataProvider.buildings[0]
+        val resource = findResource(building.resourceProducedId) ?: DataProvider.resources[0]
+
+        _uiState.update { currentState ->
+            currentState.copy(
+                selectedBuilding = building,
+                selectedResource = resource
+            )
+        }
+
+    }
+
+    private fun findResource(id: Int): Resource? {
+        return DataProvider.resources.find { resource -> resource.id == id }
+    }
+
+    private fun findBuilding(id: Int): Building? {
+        return DataProvider.buildings.find { building -> building.id == id }
+    }
+
+
+
+    fun changeView(view: View){
+        _uiState.update { currentState ->
+            currentState.copy(
+                view = view
+            )
+        }
+    }
 
 }
