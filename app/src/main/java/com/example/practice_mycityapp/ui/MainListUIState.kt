93d17ddc51e132dc5de3619ccc7f7a59a120b7ba
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-03 17:50:40 +0000

    Adds View enumerator attribute to MainListUIState data class

--- app/src/main/java/com/example/practice_mycityapp/ui/MainListUIState.kt
+++ app/src/main/java/com/example/practice_mycityapp/ui/MainListUIState.kt
@@ -3,9 +3,11 @@ package com.example.practice_mycityapp.ui
 import com.example.practice_mycityapp.model.Building
 import com.example.practice_mycityapp.model.Resource
 import com.example.practice_mycityapp.utils.ContentType
+import com.example.practice_mycityapp.utils.View
 
 data class MainListUIState (
     val selectedList: ContentType,
     val selectedBuilding: Building,
     val selectedResource: Resource,
+    val view: View
 )
